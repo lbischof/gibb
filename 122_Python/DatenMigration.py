72837ba077a55804769e07c4556e3b30273324ea
Lorenz <lorenzbischof@gmail.com>
2015-04-03 22:47:14 +0200

    Fix comments and regex

--- 122_Python/DatenMigration.py
+++ 122_Python/DatenMigration.py
@@ -9,7 +9,7 @@ patterns = [
 	'[0-9]+', # 0 AdressNr
 	'.{2,}\s.{2,}', # 1 Vorname Name
 	'[\w\' ]+\.? [0-9]+\w?', # 2 Strasse Hausnummer
-	'\d{4,5} [A-Z][a-zäüö]+', # 3 PLZ Ort
+	'\d{4,5} [A-Z]\w+', # 3 PLZ Ort
 	'((0{1,2}|\+)?41\s?|0)([1-9]\d)(\s|\s\/\s)?(\d{3})\s?(\d{2})\s?(\d{2})', # 4 Telefonnummer
 	'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}', # 5 Emailadresse
 	'(([0-9]|[0-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[0-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])', # 6 IP4 Adresse
@@ -17,7 +17,7 @@ patterns = [
 	]
 def validate(line):
 	for i,col in enumerate(line): # Go through each field in the line
-		if not re.match(patterns[i]+'$', col): # If it fails then yield the error
+		if not re.match(patterns[i]+'$', col): # If it fails then yield the value
 			yield col
 
 with open('Data1.csv', newline='') as f:
@@ -31,5 +31,5 @@ with open('Data1.csv', newline='') as f:
 			print("{}Fehler auf Zeile {}:".format(FAIL, lineNumber))
 			errors = validate(line) # Get the errors again because any() ate the first element
 			for error in errors:
-				print(error or "leer")
-			print(ENDC)
+				print(error or "leer") # print error or "leer" if empty
+			print(ENDC) # End color and insert newline
